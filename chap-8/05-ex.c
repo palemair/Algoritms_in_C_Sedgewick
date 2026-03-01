9914f96af23a728f462f107fecf12fbb7e7944b9
palemair <149162686+palemair@users.noreply.github.com>
2026-03-01 23:49:56 +0100

    Update 05-ex.c

--- chap-8/05-ex.c
+++ chap-8/05-ex.c
@@ -4,7 +4,7 @@
 /* par insertion autre que celui évoqué pour le tri shell. */
 
 /* Il peut être difficile de déterminer à l'avance une valeur sentinelle, par exemple la plus faible, */ 
-/*    qui elle même faire partie des éléments à trier. */
+/*    qui elle même pourrait faire partie des éléments à trier. */
    
 /* De plus il est peu pratique d'intégrer cette valeur sentinelle dans le tableau si on doit le trier en place. */
 
