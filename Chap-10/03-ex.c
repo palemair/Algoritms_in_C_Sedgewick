7321ffb016b48199ef0588c1feeb1e6ecadd30e4
palemair <palemair@gmail.com>
2026-04-22 07:34:10 +0200

    3bis

--- Chap-10/03-ex.c
+++ Chap-10/03-ex.c
@@ -8,23 +8,6 @@
 
 #define BITS(v,p) ((v>>p) & ~(~0U<<1U))
 
-void bin(char x)
-{
-    unsigned int b = ~(~0U >> 1);
-    int u = 0;
-    b = b >> 24;
-    while (b > 0)
-    {
-        if (u % 4 == 0 && u != 0) putchar (' ');
-
-        putchar ((b & x) ? '1' : '0');
-
-        b = b >> 1;
-        u++;
-    }
-    printf ("\n");
-}
-
 bool isBitEqual(int *tab, int b, int l,int r)
 {
     unsigned int q = BITS(tab[l],b);
@@ -39,13 +22,12 @@ void tri_echange(int *tab,int b,int l,int r)
     int i, j;
     int q;
 
-    if(isBitEqual(tab,b,l,r)&& b>0)
+    if(isBitEqual(tab,b,l,r)&& b>8)
     {
         tri_echange(tab,b-1,l,r);
     }
     else
     {
-         
         if(r>l && b>=0)
         {
          i = l; j = r;
@@ -85,7 +67,7 @@ int main()
 67  66  69  46  24  4   94  90  25  1   73  32  37  63  30  38  74  64  36  83  58  72  62  97  86  84  13  19  88  50  33  44  61  49  98  55  76  51  92  27  82  29  28  10  89  2   26  96  68  99  9   35  18  5   56  59  79  6   34  15  8   47  21  93  12  60  95  39  52  43  42  23  65  85  70  3   80  77  31  14  7   40  78  54  41  71  100 48  22  81  17  20  75  16  87  11  53  57  91  45  
 1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26  27  28  29  30  31  32  33  34  35  36  37  38  39  40  41  42  43  44  45  46  47  48  49  50  51  52  53  54  55  56  57  58  59  60  61  62  63  64  65  66  67  68  69  70  71  72  73  74  75  76  77  78  79  80  81  82  83  84  85  86  87  88  89  90  91  92  93  94  95  96  97  98  99  100 
   *
-  * Dans le cas de caractères ou de petits nombres, les bits de poids forts sont identiques,
+  * Dans le cas de caractères ou de petits nombres, les bits de poids forts sont identiques et de valeur  0,
   * donc il est inutile d'essayer de les ordonner.
   *
 */
